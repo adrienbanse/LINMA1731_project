78d9bd4a9106561fefe1f7f19e06422db5731c11
adrienbanse <adrien.banse@gmail.com>
2020-05-06 11:19:25 +0200

    vizualization + y_(t+)1

--- source/PART2.py
+++ source/PART2.py
@@ -7,13 +7,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from elevationMap import ElevationMap
+import random
 import io
-
-def pdf_e(e):
-    return 1/(np.sqrt(var_e*2*np.pi)) * np.exp(-(e-mu_e)**2/(2*var_e))
     
-
-# 1. parameters
+# 1. data and parameters
 
 with open('measures1D.txt', 'rb') as f:
     clean_lines = (line.replace(b'[',b',') for line in f)
@@ -21,27 +18,31 @@ with open('measures1D.txt', 'rb') as f:
     clean_lines = (line.replace(b' ',b'') for line in clean_lines)
     [Y_t,POSITION_t] = np.genfromtxt(clean_lines, dtype=str, delimiter=',')   
     
-Y_t         = np.array(Y_t[1:len(Y_t)-1],dtype=float)
-POSITION_t  = np.array(POSITION_t[1:len(POSITION_t)-1],dtype=float)
-Map         = ElevationMap("Ardennes.txt")
+Y_t         = np.array(Y_t[1:len(Y_t)-1],dtype=float)               # true data
+POSITION_t  = np.array(POSITION_t[1:len(POSITION_t)-1],dtype=float) # true data
 
-v_t     = 1.6   # speed
-d_t     = 0.01  # time elapsed between measures
-var_w   = 0.004 # variance of w_t for each t
-mu_w    = 0     # mean of w_t for each t
-var_e   = 16    # variance of e_t for each t
-mu_e    = 0     # mean of e_t for each t
-N       = 100   # number of particles
-T       = 50    # final time
+Map         = ElevationMap("Ardennes.txt") # Map object --> h method
 
-X_t     = np.zeros((N,T))
-X_tilde = np.zeros((N,T))
-m_x     = np.zeros(T)
+v_t         = 1.6   # speed
+d_t         = 0.01  # time elapsed between measures
+var_w       = 0.004 # variance of w_t for each t
+mu_w        = 0     # mean of w_t for each t
+var_e       = 16    # variance of e_t for each t
+mu_e        = 0     # mean of e_t for each t
+N           = 100   # number of particles
+T           = 50    # final time
 
+X_t         = np.zeros((N,T)) # recolt particles
+X_tilde     = np.zeros((N,T)) # recolt predictions
+m_x         = np.zeros(T)     # recolt expected values
+weight      = np.zeros(N)     # recolt weights at each iteration
+
+pdf_e       = lambda e: 1/np.sqrt(var_e*2*np.pi) * np.exp(-0.5*((e-mu_e)/np.sqrt(var_e))**2)
+    
 # 2. sample from initial guess
 
-X_t[:,0] = np.random.uniform(0,1,N)
-m_x[0]   = 0.5
+X_t[:,0] = np.random.uniform(0,1,N) # sampling from initial uniform distribution
+m_x[0]   = 0.5                      # initial expected value
 
 # 3. iterations
 
@@ -51,19 +52,43 @@ for t in range(T-1):
     w_t = np.random.normal(mu_w,np.sqrt(var_w),N)
     X_tilde[:,t+1] = (X_t[:,t] + v_t*d_t) + w_t
     
-    # 3.2 weight update
-    weight = np.zeros(N)
+    # 3.2. weight update
     for n in range(N):
-        weight[n] = pdf_e(Y_t[t] - Map.h(float(X_tilde[n,t+1]))) # Y_t[t] = y_(t+1)
-        
+        weight[n] = pdf_e(Y_t[t+1] - Map.h(float(X_tilde[n,t+1]))) # Y_t[t] = y_(t+1)
+                
     weight = weight/np.sum(weight)
     
-    X_t[:,t+1] = np.random.choice(X_tilde[:,t+1],size=N,p=weight)
-    m_x[t+1] = np.sum(weight*X_tilde[:,t+1])
-
-plt.plot(np.abs(POSITION_t-m_x))
-#plt.plot(POSITION_t)
+    # 3.3. resampling from estimated pdf
+    X_t[:,t+1]  = np.random.choice(X_tilde[:,t+1],size=N,p=weight)
+    #m_x[t+1]    = np.sum(weight*X_tilde[:,t+1])
         
+# Visualization
+plt.figure(1)
+for t in range(T):
+    print(t)
+    # Display particles at each time:  
+    for i in range(N):
+        plt.plot(t,X_t[i,t],'ro',markersize=1)
+  
+    # Display true x at each time:
+    plt.plot(t,POSITION_t[t],'kx')
+  
+    # Compute and display sample mean for each time:
+    x_mean=0
+    for i in range(N):
+        x_mean = x_mean + X_t[i,t]
+  
+    x_mean = x_mean / N
+    plt.plot(t,x_mean,'rx')
+    
+plt.xlabel('t')
+plt.ylabel('x_t^i, i=1,...,n')
+plt.title('Sequential Monte Carlo experiment')
+plt.show()
+
+plt.figure(2)
+plt.plot(np.linspace(0,1,len(Y_t)),Y_t)
+plt.show()
     
     
     
